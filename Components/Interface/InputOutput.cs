b122ff1068f5ede55dafd5363f7f07dbc2e78be9
JakubKorytko <jakub.korytko@o2.pl>
2023-11-22 21:00:59 +0100

    Refactor InputOutput.cs for better readability

--- Components/Interface/InputOutput.cs
+++ Components/Interface/InputOutput.cs
@@ -9,25 +9,37 @@ namespace WordsGuesser.Interface
     {
         public static string HandleLetterIO(Dictionary<string, string> info, int[] missed, int misses)
         {
+            string passwordLength = "The password is "
+                + info["wordLength"]
+                + " letters long!\n";
+
+            string guessedLetters = "Guessed letters: "
+                + info["board"]
+                + "\n";
+
+            string category = "Category: "
+                + info["category"]
+                + "\n";
+
+            string guessesLeft = "You have "
+                + info["guesses_left"]
+                + " attempts left!\n";
 
             Console.WriteLine("---------\n");
-            Console.WriteLine("The password is " + info["wordLength"] + " letters long!\n");
+            Console.WriteLine(passwordLength);
+
             Console.ForegroundColor = ConsoleColor.Green;
-            Console.WriteLine("Guessed letters: " + info["board"] + "\n");
+            Console.WriteLine(guessedLetters);
+
             Console.ForegroundColor = ConsoleColor.Green;
-            Console.WriteLine("Category: " + info["category"] + "\n");
+            Console.WriteLine(category);
+
             Console.ResetColor();
-            Console.WriteLine("You have " + info["guesses_left"] + " attempts left!\n");
+            Console.WriteLine(guessesLeft);
+
             Console.Write("Missed letters: ");
-            int letters_print = 0;
-            if (misses != 0) foreach (int miss in missed)
-                {
-                    if (miss == 0) continue;
-                    letters_print++;
-                    Console.Write(Convert.ToChar(miss));
-                    if (letters_print != misses) Console.Write(",");
-                }
-            else Console.Write("none");
+
+            DisplayMissedLetters(missed, misses);
 
             Console.Write("\nGuess the letter: ");
 
@@ -39,27 +51,57 @@ namespace WordsGuesser.Interface
         public static int HandleEndGameIO(bool won, string word)
         {
             Console.Clear();
-            Console.WriteLine("The password was: " + word + "\n");
+            Console.WriteLine(
+                "The password was: "
+                + word
+                + "\n"
+            );
 
             Console.ForegroundColor = ConsoleColor.Yellow;
-            if (won) Console.WriteLine("You won! ");
-            else Console.WriteLine("You've run out of attempts... ");
+            if (won)
+                Console.WriteLine("You won! ");
+            else
+                Console.WriteLine("You've run out of attempts... ");
+
             Console.ResetColor();
-
             Console.WriteLine("Do you want to play again?\n");
+
             Console.ForegroundColor = ConsoleColor.Yellow;
-            Console.WriteLine("1. Yes\n2. No\n");
+            Console.WriteLine("1. Yes");
+            Console.WriteLine("2. No\n");
+
             Console.ForegroundColor = ConsoleColor.Green;
             Console.Write("Choice: ");
+            
             Console.ResetColor();
 
-            int decision = 3;
+            int decision = 0;
 
-            while (decision != 1 && decision != 2) int.TryParse(Console.ReadLine(), out decision);
+            while (decision != 1 && decision != 2)
+                int.TryParse(Console.ReadLine(), out decision);
 
             Console.Clear();
 
             return decision;
+        }
+
+        private static void DisplayMissedLetters(int[] missed, int misses)
+        {
+            int letters_print = 0;
+
+            if (misses != 0)
+                foreach (int miss in missed)
+                {
+                    if (miss == 0)
+                        continue;
+
+                    Console.Write(Convert.ToChar(miss));
+
+                    if (++letters_print != misses)
+                        Console.Write(",");
+                }
+            else
+                Console.Write("none");
         }
 
         public static void DisplayError(Errors.ERROR_CODES code)
